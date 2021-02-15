de96e0acbfd56b9d109147c6342d61f082bba440
Aurelie75FR <aurelielo.vi@gmail.com>
2021-02-15 10:20:19 +0100

    the doors animation done

---
+++ gameapp.js
@@ -0,0 +1,17 @@
+const door1 = document.querySelector(".door-One");
+const door2 = document.querySelector(".door-Two");
+const door3 = document.querySelector(".door-Three");
+
+door1.addEventListener("click", toggleDoor1);
+door2.addEventListener("click", toggleDoor2);
+door3.addEventListener("click", toggleDoor3);
+
+function toggleDoor1() {
+  door1.classList.toggle("doorOpen");
+}
+function toggleDoor2() {
+  door2.classList.toggle("doorOpen");
+}
+function toggleDoor3() {
+  door3.classList.toggle("doorOpen");
+}
