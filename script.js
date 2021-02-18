89cad8f986fb6be2e48285e92b8f8d40467995a9
Aurelie75FR <aurelielo.vi@gmail.com>
2021-02-18 09:23:17 +0100

    massive issues

--- script.js
+++ script.js
@@ -1,51 +1,60 @@
+const nextDay = document.getElementById("generateNewDay")
 const doors = document.querySelectorAll(".back");
-const start = document.getElementById("gamestart");
-const frontDoor = document.querySelectorAll(".front");
+const start = document.getElementById("gameStart");
+const jumpscare = document.getElementById("gameOver");
 let rooms = 0;
 let score = 0;
 shuffle();
 
+// function gamePlay(){
+//   let init = gameStart()
+//   // let play = 
+// }
+
+
 function shuffle() {
   let allDoors = document.getElementsByClassName("img");
-  //allDoors[rooms].setAttribute("src", "./Images/dark-room.jpg");
   let random = Math.floor(Math.random() * allDoors.length);
-  console.log(random);
   let trap = allDoors[random];
   trap.setAttribute("src", "./Images/zut.gif");
-  // rooms = random;
-}
+ }
 
 function setScore() {
   const scoreText = document.getElementById("score");
   score++;
   scoreText.innerHTML = score;
+  // if(score = 5){
+  //   // endGame()
+  // }
   console.log(score);
 }
+
+function youLose() {
+  jumpscare.classList.toggle("hide");
+}
+jumpscare.addEventListener("click", youLose);
 
 function toggleDoor(evt) {
   const parentDoor = evt.target.parentElement;
   const img = parentDoor.querySelector(".img");
   console.log(img.src);
   if (img.src === "http://127.0.0.1:5500/Images/zut.gif") {
+    youLose();
     console.log("dead");
   } else {
     setScore();
     console.log("tout va bien");
   }
-
-  //ecrire condition pour déterminer si img === win ou loose
   evt.target.classList.toggle("doorOpen");
 }
-
-function lose() {}
 
 function gameStart(evt) {
   evt.target.classList.toggle("hide");
 }
 start.addEventListener("click", gameStart);
 
-function gameOver() {}
-
 doors.forEach(function (doorMove) {
   doorMove.addEventListener("click", toggleDoor);
 });
+// nextDay.addEventListener('click',shuffle)
+// console.log(nextDay)
